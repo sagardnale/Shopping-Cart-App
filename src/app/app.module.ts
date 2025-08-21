0e98ed46ae392adf49118bface5e3fa4ccc625aa
sagardnale <nalesagard@gmail.com>
2025-08-21 12:28:50 +0530

    created components like  navbar,cart,products and service cart for shopping cart app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ProductsComponent } from './component/products/products.component';
+import { CartComponent } from './component/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
