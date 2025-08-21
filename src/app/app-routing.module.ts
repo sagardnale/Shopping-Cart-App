524e47f45d325fc3a4f622b81ec29ee4fb2ed44a
sagardnale <nalesagard@gmail.com>
2025-08-21 15:51:03 +0530

    added routing to product and cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './component/products/products.component';
+import { CartComponent } from './component/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products',component:ProductsComponent},
+  {path:'cart',component:CartComponent},
+  {path:'',redirectTo:'/products',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
