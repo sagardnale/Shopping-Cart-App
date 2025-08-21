8ad3e8f49d3f5ece49eb2bacbcade062003a026f
sagardnale <nalesagard@gmail.com>
2025-08-21 16:43:17 +0530

    created Ui for products

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+    products = [
+    { id: 1, name: 'Nike Shoes', price: 120, img: 'https://picsum.photos/200/150?random=1' },
+    { id: 2, name: 'Adidas Sneakers', price: 100, img: 'https://picsum.photos/200/150?random=2' },
+    { id: 3, name: 'Puma Running', price: 90, img: 'https://picsum.photos/200/150?random=3' },
+    { id: 4, name: 'Reebok Classic', price: 110, img: 'https://picsum.photos/200/150?random=4' }
+  ];
+  constructor(public readonly cartService:CartService){
 
+  }
+
+  add( product:any){
+    this.cartService.addToCart(product);
+    alert(`${product.name} added to cart!`);
+  }
 }
